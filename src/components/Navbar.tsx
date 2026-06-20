d2bf0aa3b098660b04391b2a2107cf8c723de4a0
Wael <wael@example.com>
2026-06-21 01:35:26 +0300

    feat: update Lanyard and Navbar components
    - Update Lanyard with new card.glb and configuration
    - Enhance Navbar with navigation improvements

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,10 +31,10 @@ const Navbar = () => {
   return (
 
     <motion.header
-      className="absolute top-5  z-1   "
+      className="absolute top-5 sticky  w-fit z-1   "
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.6, ease: [0.22, 1, 0.36, 1] }}
+      transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
     >
       <nav
         className={cn(
